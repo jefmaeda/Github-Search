17fa86b6c6f2e8a5fbf75467372574784ca6ac04
Jef <jefersontamono@gmail.com>
2023-05-05 07:32:00 +0900

    feat(conect API): enabled frist conection the github IPA

--- js/favorites.js
+++ js/favorites.js
@@ -8,11 +8,30 @@ unity of class is "heritage -> extends"
 this responsible for creating the HTML
 */
 
+//class 0
+export class GithubUser{
+  static search(username){
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(data => ({
+      login: data.login,
+      name: data.name,
+      public_repos: data.public_repos,
+      followers: data.followers
+    }))
+  }
+}
+
 //class 1
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load()
+
+    GithubUser.search('jefmaeda')
+    .then(user => console.log(user))
   }
 
   load(){
